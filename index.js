35434395ff6209d5fd1c80550e90f3d29b2e2a00
MimFSD <mimislam878@gmail.com>
2024-09-05 00:05:02 +0600

    connected mongodb with environment variable

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 
- 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szh9b4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
