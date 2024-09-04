f7af24c47f967cc948a13e04927e24aea49687a0
MimFSD <mimislam878@gmail.com>
2024-09-05 00:08:51 +0600

    implement delete method for delete query from mongodb

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 9000;
+
+// Middlewares============
+const options = {
+    origin: [
+      'http://localhost:5173',
+      'https://altquery.web.app',
+      'https://altquery.firebaseapp.com 1QQQQQQQQQQ23W22222222222422222222222222242424222222222222222222222222222222222222222222222222222222                        ',
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(options));
+  app.use(express.json());
+  app.use(cookieParser());
+  // Veryfy JWT token
+  const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    // console.log(token);
+    if (!token) {
+      return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    if (token) {
+      jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+          console.log(err);
+          return res.status(401).send({ message: 'Unauthorized access' });
+        }
+        // console.log(decoded);
+        req.user = decoded;
+        next();
+      });
+    }
+  };
+
+  
