ee8fe669e69406b4398e308430117d49623505d5
MimFSD <mimislam878@gmail.com>
2024-09-05 00:09:42 +0600

    chached node_modules

--- index.js
+++ index.js
@@ -40,4 +40,119 @@ const options = {
     }
   };
 
-  
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htex290.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+    const queriesCallection = client.db('altQueryDB').collection('queries');
+    const recommendationCallection = client
+      .db('altQueryDB')
+      .collection('recommendation');
+
+    // JWT Genaret TOKEN and added cookie
+    app.post('/jwt', async (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.SECRET_TOKEN, {
+        expiresIn: '1d',
+      });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          // sameSite: 'none',
+        })
+        .send({ success: true });
+    });
+    // Remove token form cookie ============
+    app.get('/logout', (req, res) => {
+      res
+        .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.MODE_ENV === 'production' ? 'none' : 'strict',
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
+    // Queries added to database
+    app.post('/queries', async (req, res) => {
+      const data = req.body;
+      // console.log(data);
+      const result = await queriesCallection.insertOne(data);
+      res.send(result);
+    });
+    //  get only 8 data from database
+    app.get('/latest-queries', async (req, res) => {
+      const data = await queriesCallection
+        .find()
+        .sort({ _id: -1 })
+        .limit(8)
+        .toArray();
+      res.send(data);
+    });
+    // Get All query data from database
+    app.get('/all-queries', async (req, res) => {
+      const searchs = req.query.searchs;
+      const size = req.query.size;
+      const page = parseInt(req.query.page) - 1;
+      const query = {
+        productName: { $regex: searchs, $options: 'i' },
+      };
+      // console.log(size, page);
+      const data = await queriesCallection
+        .find(query)
+        .skip(page * parseInt(size))
+        .limit(parseInt(size))
+        .sort({ _id: -1 })
+        .toArray();
+      res.send(data);
+    });
+
+    // Get All query data length from database
+    app.get('/all-queries-len', async (req, res) => {
+      const searchs = req.query.searchs;
+      const query = {
+        productName: { $regex: searchs, $options: 'i' },
+      };
+      const data = await queriesCallection.countDocuments(query);
+      res.send({ data });
+    });
+    //  get only my added query data from database
+    app.get('/my-queries/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const tokenEmail = req.user.email;
+      if (tokenEmail !== email) {
+        return res.status(4033).send({ message: 'Forbidden access' });
+      }
+      const query = { userEmail: email };
+      const data = await queriesCallection
+        .find(query)
+        .sort({ _id: -1 })
+        .toArray();
+      res.send(data);
+    });
+
+    // Get only select query details data from database
+    app.get('/query-details/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await queriesCallection.findOne(query);
+      res.send(result);
+    });
